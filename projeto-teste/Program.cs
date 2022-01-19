e4859bcf56f49650226438c39f8855ce5aa0ef41
Rodrigo Fogaça <rodrigofogacars@gmail.com>
2022-01-18 21:49:05 -0300

    Acrescentada a mensagem desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Bom dia!");
 Console.WriteLine("Como você está hoje?");
+Console.WriteLine("Tenha um bom dia");
